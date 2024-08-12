0ccf0bab7f20906f3f6612def4b83ab073065797
Hassan Rahman <hr-awesome@hotmail.com>
2024-08-12 16:28:17 -0500

    First test on translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +7,4 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'orthodox-web';
-
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang('en');
-  }
-
-  switchLanguage(language: string) {
-    this.translate.use(language);
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+  }
 
+  switchLanguage(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const selectedLanguage = target?.value;
+
+    if (selectedLanguage) {
+      this.translate.use(selectedLanguage);
+      console.log('Selected language:', selectedLanguage);
+    }
+  }
 }
