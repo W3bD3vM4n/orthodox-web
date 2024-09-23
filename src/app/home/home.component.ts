99938d6ea236777013a5cbc7514baa6c09f1ac0d
Hassan Rahman <hr-awesome@hotmail.com>
2024-09-23 12:17:49 -0500

    Fix layout for News, p4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NewsService } from '../news/news.service';
+import { News } from '../news/news.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  newsList: News[] = [];
+  maxNewsItems: number = 2; // Limita el número de noticias mostradas
+
+  constructor(private router: Router, private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.newsList = this.newsService.getNewsList();
+  }
+
+  getSummary(content: string): string {
+    // Verifica que el contenido no sobrepase a 200 palabras
+    let summary = content.slice(0, 200);
+
+    // Encuentra el ultimo espacio para evitar cortar una palabra
+    const lastSpaceIndex = summary.lastIndexOf(' ');
+
+    // Si hay un espacio entre las 200 palabras lo trunca
+    if (lastSpaceIndex !== -1) {
+        summary = summary.slice(0, lastSpaceIndex);
+    }
+
+    // Verifica si el resumen termina con punto sino le agrega tres
+    if (!summary.endsWith('.')) {
+        summary += '...';
+    }
+
+    return summary;
+  }
+
+  openNews(id: number) {
+    this.router.navigate(['/news', id]);
+  }
 }
--- src/app/news/news.service.ts
+++ src/app/news/news.service.ts
@@ -19,6 +19,12 @@ export class NewsService {
       date: 'September 19, 2024',
       content: 'Vivamus ac faucibus arcu. Etiam vel pretium lectus. Nunc dui lectus, luctus sed porttitor in, condimentum sed nunc. Mauris gravida porttitor eros quis pulvinar. Etiam felis tellus, pharetra vel euismod sit amet, faucibus quis purus. Integer condimentum tempor lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum vel urna eget tellus pulvinar efficitur. Sed sollicitudin mi eget consectetur vehicula. Suspendisse risus arcu, dapibus vitae venenatis sit amet, pellentesque sed lectus. Morbi luctus metus quis odio finibus finibus. Nunc ornare commodo turpis nec auctor. Proin elit nibh, laoreet sed nisi a, eleifend faucibus tellus. Vivamus quis mauris odio. Etiam non ligula quis erat finibus efficitur vitae vel erat.'
     },
+    {
+      id: 3,
+      title: 'Noticia #3',
+      date: 'September 17, 2024',
+      content: 'Mauris non neque sodales, suscipit diam sit amet, varius augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur non ultrices sapien. Aenean lorem ex, pellentesque nec ipsum ut, suscipit suscipit sem. Mauris placerat dignissim lacus et rhoncus. Aliquam nec pellentesque lacus, ac sodales tellus. Morbi tincidunt ipsum interdum mauris pellentesque porttitor. Nullam quis enim ut felis congue efficitur quis in libero. Ut vulputate, ipsum in vehicula venenatis, felis nulla accumsan tortor, non facilisis sapien libero quis tellus. Mauris sit amet justo sapien.'
+    },
   ];
 
   constructor() {}
